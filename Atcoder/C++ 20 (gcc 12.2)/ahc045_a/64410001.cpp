a0e67b6ac2de921caac3329e918d37bd554f2a58
Vicente Opazo <65175601+visho33@users.noreply.github.com>
2025-11-07 23:46:36 -0300

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/ahc045_a/64410001.cpp
@@ -0,0 +1,259 @@
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#if defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#endif
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+long double cpu_time() {
+#if defined(_WIN32) || defined(_WIN64)
+    FILETIME creation_ft, exit_ft, kernel_ft, user_ft;
+    GetProcessTimes(GetCurrentProcess(), &creation_ft, &exit_ft, &kernel_ft, &user_ft);
+     
+    auto extract_time = [](FILETIME ft) {
+        return 1e-7L * (ft.dwLowDateTime | uint64_t(ft.dwHighDateTime) << 32);
+    };
+     
+    return extract_time(user_ft) + extract_time(kernel_ft);
+#else
+    return (long double) clock() / CLOCKS_PER_SEC;
+#endif
+}
+
+int N, M, Q, L, W;
+vector<int> G;
+vector<int> lx, rx, ly, ry;
+vector<bool> listo;
+vector<vector<int> > nodos;
+vector<vector<ii> > aristas;
+
+ll dis(int i, int j){
+    return ((lx[i] + rx[i]) - (lx[j] + rx[j]))*((lx[i] + rx[i]) - (lx[j] + rx[j])) + ((ly[i] + ry[i]) - (ly[j] + ry[j]))*((ly[i] + ry[i]) - (ly[j] + ry[j]));;
+}
+
+double dis2(int i, int j){
+    double x1 = (double(lx[i]) + double(rx[i]))/double(2);
+    double x2 = (double(lx[j]) + double(rx[j]))/double(2);
+    double y1 = (double(ly[i]) + double(ry[i]))/double(2);
+    double y2 = (double(ly[j]) + double(ry[j]))/double(2);
+    return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+}
+
+void intentar(int i, int j){
+
+    vector<int> disponible;
+
+    for(auto u: nodos[i]){
+        disponible.push_back(u);
+        listo[u] = false;
+    }
+
+    for(auto u: nodos[j]){
+        disponible.push_back(u);
+        listo[u] = false;
+    }
+
+    random_shuffle(disponible.begin(), disponible.end());
+
+    vector<int> bnodosi = nodos[i];
+    vector<int> bnodosj = nodos[j];
+    vector<ii> baristasi = aristas[i];
+    vector<ii> baristasj = aristas[j];
+    
+    ll sumantes = 0;
+    for(auto u: aristas[i]){
+        sumantes += dis(u.first, u.second);
+    }
+    for(auto u: aristas[j]){
+        sumantes += dis(u.first, u.second);
+    }
+
+    nodos[i].clear();
+    nodos[j].clear();
+    aristas[i].clear();
+    aristas[j].clear();
+
+    vector<int> voy = {i, j};
+    vector<int> iniciox = {-1, -1};
+
+    int maxi = -1;
+
+    for(auto u: disponible){
+        for(auto v: disponible){
+            if(dis(u, v) > maxi){
+                maxi = dis(u, v);
+                iniciox[0] = u;
+                iniciox[1] = v;
+            }
+        }
+    }
+
+    listo[iniciox[0]] = true;
+    listo[iniciox[1]] = true;
+
+    for(int r = 0; r<2; r++){
+        int gx = voy[r];
+        int g = G[gx];
+        priority_queue<vector<ll>, vector<vector<ll> >, greater<vector<ll> > > q;
+        int inicio = iniciox[r];
+        nodos[gx].push_back(inicio);
+        listo[inicio] = true;
+        for(int i: disponible){
+            if(listo[i] == true) continue;
+            q.push({dis(inicio, i), inicio, i});
+        }
+        while(q.size() > 0 && int(nodos[gx].size()) < g){
+            int x = q.top()[1];
+            int y = q.top()[2];
+            q.pop();
+            if(listo[y] == true) continue;
+            nodos[gx].push_back(y);
+            aristas[gx].push_back({x, y});
+            listo[y] = true;
+            for(int i: disponible){
+                if(listo[i] == true) continue;
+                q.push({dis(y, i), y, i});
+            }
+        }
+    }
+
+    ll sum = 0;
+    for(auto u: aristas[i]){
+        sum += dis(u.first, u.second);
+    }
+    for(auto u: aristas[j]){
+        sum += dis(u.first, u.second);
+    }
+
+    if(sumantes < sum){
+        nodos[i] = bnodosi;
+        nodos[j] = bnodosj;
+        aristas[i] = baristasi;
+        aristas[j] = baristasj;
+    }
+
+    return;
+}
+
+int main(){
+
+    cin>>N>>M>>Q>>L>>W;
+
+    G.resize(M);
+
+    for(int i = 0; i<M; i++){
+        cin>>G[i];
+    }
+
+    lx.resize(N);
+    rx.resize(N);
+    ly.resize(N);
+    ry.resize(N);
+
+    for(int i = 0; i<N; i++){
+        cin>>lx[i]>>rx[i]>>ly[i]>>ry[i];
+    }
+
+    listo.resize(N, false);
+    nodos.resize(M);
+    aristas.resize(M);
+
+    for(int gx = 0; gx<M; gx++){
+        int g = G[gx];
+        priority_queue<vector<ll>, vector<vector<ll> >, greater<vector<ll> > > q;
+        int inicio = -1;
+        for(int i = 0; i<N; i++){
+            if(listo[i] == false){
+                inicio = i;
+                break;
+            }
+        }
+        nodos[gx].push_back(inicio);
+        listo[inicio] = true;
+        for(int i = 0; i<N; i++){
+            if(listo[i] == true) continue;
+            q.push({dis(inicio, i), inicio, i});
+        }
+
+        while(q.size() > 0 && int(nodos[gx].size()) < g){
+            int x = q.top()[1];
+            int y = q.top()[2];
+            q.pop();
+            if(listo[y] == true) continue;
+            nodos[gx].push_back(y);
+            aristas[gx].push_back({x, y});
+            listo[y] = true;
+            for(int i = 0; i<N; i++){
+                if(listo[i] == true) continue;
+                q.push({dis(y, i), y, i});
+            }
+        }
+    }
+
+    while(cpu_time() < 1.4){
+        int i = rng()%M;
+        int j = rng()%M;
+        if(i == j) continue;
+        intentar(i, j);
+    }
+
+    vector<int> cual(N);
+
+    for(int i = 0; i<M; i++){
+        for(auto u: nodos[i]){
+            cual[u] = i;
+        }
+    }
+
+    while(Q > 0 && cpu_time() < 1.9){
+
+        int i = rng()%N;
+        int grupo = cual[i];
+
+        vector<int> qnodos = {i};
+        vector<int> qaristas;
+
+        for(int j = 0; j<int(aristas[grupo].size()); j++){
+            ii u = aristas[grupo][j];
+            if(int(qnodos.size()) == L) break;
+            if(u.first == i){
+                qnodos.push_back(u.second);
+                qaristas.push_back(j);
+            }
+            if(u.second == i){
+                qnodos.push_back(u.first);
+                qaristas.push_back(j);
+            }
+        }
+
+        if(int(qnodos.size()) <= 2) continue;
+
+        cout<<"? "<<qnodos.size()<<" ";
+        for(auto u: qnodos){
+            cout<<u<<" ";
+        }
+        cout<<endl;
+
+        for(auto u: qaristas){
+            cin>>aristas[grupo][u].first>>aristas[grupo][u].second;
+        }
+        Q--;
+    }
+
+    cout<<"!"<<endl;
+
+    for(int gx = 0; gx<M; gx++){
+        for(auto u: nodos[gx]){
+            cout<<u<<" ";
+        }
+        cout<<endl;
+        for(auto u: aristas[gx]){
+            cout<<u.first<<" "<<u.second<<endl; 
+        }
+    }
+
+    return 0;
+}
